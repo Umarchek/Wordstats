ddd1395ff0a56fdbe346769c61bc262813b9d6b6
UZBase <sharofiddinov.07@mail.ru>
2021-10-30 19:01:52 +0500

    addded

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ const searchButton = document.querySelector('.searchButton')
 const searchValue = document.querySelector('.searchTerm')
 const mainInfoDiv = document.querySelector('.main-info-div');
 const maintext = document.querySelector('#main_title');
+const maintitle_discription = document.querySelector('#main_title_discription');
 searchButton.addEventListener('click', () => {
     mainInfoDiv.style.display = 'block'
     maintext.innerHTML = searchValue.value
+    const randomNumber = Math.floor(Math.random() * 2000)
+    maintitle_discription.innerHTML = randomNumber
     searchValue.value = ''
 })
