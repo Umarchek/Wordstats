8447831d8adb730574524842d63c3dd876b976b2
UZBase <sharofiddinov.07@mail.ru>
2021-10-30 18:22:44 +0500

    mobile version end !!!!

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 const searchButton = document.querySelector('.searchButton')
 const searchValue = document.querySelector('.searchTerm')
+const mainInfoDiv = document.querySelector('.main-info-div');
+const maintext = document.querySelector('#main_title');
+searchButton.addEventListener('click', () => {
+    mainInfoDiv.style.display = 'block'
+    maintext.innerHTML = searchValue.value
+    searchValue.value = ''
+})
